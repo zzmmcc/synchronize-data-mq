e60bc80d047d495fd43f900b6bbbe02d2261c57f
zhangMC <1129414986@qq.com>
2022-05-13 16:48:47 +0800

    first commit

---
+++ src/main/java/com/zhangmc/SynchronizeDataMqApplication.java
@@ -0,0 +1,13 @@
+package com.zhangmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynchronizeDataMqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SynchronizeDataMqApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhangmc/SynchronizeDataMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhangmc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynchronizeDataMqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
